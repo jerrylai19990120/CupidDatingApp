a09689aa076383294303dc88d62b9c632fc59bec
jerry <jerrylai19990120@gmail.com>
2021-02-24 15:15:26 -0500

    setting update

--- CupidDating/ComponentViews/SettingsOptions.swift
+++ CupidDating/ComponentViews/SettingsOptions.swift
@@ -12,42 +12,132 @@ struct SettingsOptions: View {
     
     var gr: GeometryProxy
     
+    @State var isOn: Bool = true
+    @State var distanceRange:Double = 20
+    @State var ageRange:Double = 26
+    
     var body: some View {
         
         List {
             
-            Section(header: Text("Discovery settings")) {
-                Text("DISCOVERY SETTINGS").foregroundColor(Color(red: 245/255, green: 39/255, blue: 119/255))
-                    .font(.system(size: 18, weight: .semibold, design: .default))
-                Text("DISCOVERY SETTINGS").foregroundColor(Color(red: 245/255, green: 39/255, blue: 119/255))
-                .font(.system(size: 18, weight: .semibold, design: .default))
+            Section(header: SectionHeader(header: "DISCOVERY SETTINGS")) {
+                HStack {
+                    Text("My Location")
+                        .font(.system(size: 20, weight: .regular, design: .default))
+                    Spacer()
+                    VStack(spacing: 2) {
+                        Text("My Current Location")
+                        .font(.system(size: 20, weight: .regular, design: .default))
+                            .foregroundColor(Color(red: 108/255, green: 108/255, blue: 108/255))
+                        
+                        Text("Toronto, ON")
+                        .font(.system(size: 20, weight: .regular, design: .default))
+                            .foregroundColor(.gray)
+                    }
+                }
+                
+                VStack {
+                    HStack {
+                        Text("Maximum Distance")
+                            .font(.system(size: 20, weight: .regular, design: .default))
+                        Spacer()
+                        Text("\(Int(self.distanceRange))km")
+                            .font(.system(size: 20, weight: .regular, design: .default)).foregroundColor(.gray)
+
+                        
+                    }
+                    
+                    Slider(value: $distanceRange, in: 0...80)
+                }
+                
+                VStack {
+                    HStack {
+                        Text("Age Range")
+                            .font(.system(size: 20, weight: .regular, design: .default))
+                        Spacer()
+                        Text("\(Int(self.ageRange))")
+                            .font(.system(size: 20, weight: .regular, design: .default)).foregroundColor(.gray)
+
+                        
+                    }
+                    Slider(value: $distanceRange, in: 18...60)
+                }
+               
+                
+            }
+            
+            Section(header: Text("Show Me:").font(.system(size: 13, weight: .semibold, design: .default))) {
+                Toggle(isOn: self.$isOn) {
+                    Text("Men")
+                    .font(.system(size: 20, weight: .regular, design: .default))
+                }.padding([.top, .bottom])
+                
+                Toggle(isOn: self.$isOn) {
+                    Text("Women")
+                    .font(.system(size: 20, weight: .regular, design: .default))
+                }.padding([.top, .bottom])
+            }
+            
+            Section(header: Text("")) {
+                Toggle(isOn: self.$isOn) {
+                    Text("Show me on Cupid")
+                    .font(.system(size: 20, weight: .regular, design: .default))
+                }.padding([.top, .bottom])
+            }
+            
+            
+            Section(header: Text("")) {
+                HStack {
+                    Spacer()
+                    Text("Help & Support")
+                        .font(.system(size: 20, weight: .regular, design: .default)).padding()
+                    Spacer()
+                }
 
             }
             
-            Section(header: Text("Discovery settings")) {
-                Text("DISCOVERY SETTINGS").foregroundColor(Color(red: 245/255, green: 39/255, blue: 119/255))
-                    .font(.system(size: 18, weight: .semibold, design: .default))
-                Text("DISCOVERY SETTINGS").foregroundColor(Color(red: 245/255, green: 39/255, blue: 119/255))
-                .font(.system(size: 18, weight: .semibold, design: .default))
+            
+            Section(header: Text("")) {
+                HStack {
+                    Spacer()
+                    Text("Share Cupid")
+                        .font(.system(size: 20, weight: .regular, design: .default)).padding()
+                    Spacer()
+                }
+                
 
             }
             
             
-            Section(header: Text("Discovery settings")) {
-                Text("DISCOVERY SETTINGS").foregroundColor(Color(red: 245/255, green: 39/255, blue: 119/255))
-                    .font(.system(size: 18, weight: .semibold, design: .default))
-                Text("DISCOVERY SETTINGS").foregroundColor(Color(red: 245/255, green: 39/255, blue: 119/255))
-                .font(.system(size: 18, weight: .semibold, design: .default))
+
+            Section(header: Text("LEGAL").font(.system(size: 13, weight: .semibold, design: .default))) {
+                NavigationLink(destination: HomeView()) {
+                    Text("Privacy Policy")
+                        .font(.system(size: 18, weight: .regular, design: .default))
+                    }.padding([.top, .bottom])
+                
+                NavigationLink(destination: HomeView()) {
+                    Text("Terms of Service")
+                        .font(.system(size: 18, weight: .regular, design: .default))
+                }.padding([.top, .bottom])
+
+                NavigationLink(destination: HomeView()) {
+                    Text("Licenses")
+                        .font(.system(size: 18, weight: .regular, design: .default))
+                }.padding([.top, .bottom])
+
 
             }
             
-            Section(header: Text("Discovery settings")) {
-                Text("DISCOVERY SETTINGS").foregroundColor(Color(red: 245/255, green: 39/255, blue: 119/255))
-                    .font(.system(size: 18, weight: .semibold, design: .default))
-                Text("DISCOVERY SETTINGS").foregroundColor(Color(red: 245/255, green: 39/255, blue: 119/255))
-                .font(.system(size: 18, weight: .semibold, design: .default))
-
+            Section(header: Text("")) {
+                HStack {
+                    Spacer()
+                    Text("Logout")
+                        .font(.system(size: 20, weight: .regular, design: .default)).padding()
+                    Spacer()
+                }
             }
+            
             
             
         }.listStyle(GroupedListStyle())
@@ -60,5 +150,15 @@ struct SettingsOptions_Previews: PreviewProvider {
         GeometryReader { gr in
             SettingsOptions(gr: gr)
         }
+    }
+}
+
+struct SectionHeader: View {
+    
+    var header: String
+    
+    var body: some View {
+        Text("\(header)").foregroundColor(Color(red: 245/255, green: 39/255, blue: 119/255))
+            .font(.system(size: 13, weight: .semibold, design: .default))
     }
 }
